a72c2e63c5c89078bb39c660d95d317c38a74920
Chris Doyle <chris@arches.io>
2017-05-13 19:14:58 -0500

    dynamic volunteer stats

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -11,6 +11,10 @@ class WelcomeController < ApplicationController
     @wards.each do |ward|
       @new_volunteers_by_ward[ward] = [0, TableEvent.for_ward(ward).during(Time.now.all_month).sum(:volunteers_collected)]
     end
+
+    @volunteers_by_ward = {}
+    @volunteers_by_ward['48'] = TableEvent.for_ward(48).during(Time.now.all_month).flat_map(&:attendees).flat_map(&:volunteer_id).uniq.length
+    @volunteers_by_ward['49'] = TableEvent.for_ward(49).during(Time.now.all_month).flat_map(&:attendees).flat_map(&:volunteer_id).uniq.length
   end
 end
 
