3e10f89485da51bf37f5ffa7ab948826349cdc38
Chris Doyle <chris@arches.io>
2017-05-21 14:40:38 -0500

    style header and messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  add_flash_types :success, :error
+
   def require_superuser!
     authenticate_user!
 
