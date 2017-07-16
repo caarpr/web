18c1508cf9332c23035a299f18a45fac15118c22
Chris Doyle <chris@arches.io>
2017-07-15 20:11:14 -0500

    charts show all data

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,19 +2,44 @@ class WelcomeController < ApplicationController
   def index
     @wards = TableLocation.pluck("distinct ward")
 
+    t0 = TableEvent.minimum(:starts_at).beginning_of_month
+
+    @labels = []
+    loop_until_now(t0) do |t|
+      @labels << t.strftime("%B")
+    end
+
     @postcards_by_ward = {}
     @wards.each do |ward|
-      @postcards_by_ward[ward] = [0, TableEvent.for_ward(ward).during(Time.now.all_month).sum(:postcards_collected)]
+      @postcards_by_ward[ward] = []
+      loop_until_now(t0) do |t|
+        @postcards_by_ward[ward] << TableEvent.for_ward(ward).during(t.all_month).sum(:postcards_collected)
+      end
     end
 
     @new_volunteers_by_ward = {}
     @wards.each do |ward|
-      @new_volunteers_by_ward[ward] = [0, TableEvent.for_ward(ward).during(Time.now.all_month).sum(:volunteers_collected)]
+      @new_volunteers_by_ward[ward] = []
+      loop_until_now(t0) do |t|
+        @new_volunteers_by_ward[ward] << TableEvent.for_ward(ward).during(t.all_month).sum(:volunteers_collected)
+      end
     end
 
-    @volunteers_by_ward = {}
-    @volunteers_by_ward['48'] = TableEvent.for_ward(48).during(Time.now.all_month).flat_map(&:attendees).flat_map(&:volunteer_id).uniq.length
-    @volunteers_by_ward['49'] = TableEvent.for_ward(49).during(Time.now.all_month).flat_map(&:attendees).flat_map(&:volunteer_id).uniq.length
+    @table_staff_by_ward = {}
+    @wards.each do |ward|
+      @table_staff_by_ward[ward] = []
+      loop_until_now(t0) do |t|
+        @table_staff_by_ward[ward] << TableEvent.for_ward(ward).during(t.all_month).flat_map(&:attendees).flat_map(&:volunteer_id).uniq.length
+      end
+    end
+  end
+
+  def loop_until_now(start_time)
+    t = start_time.beginning_of_month
+    while t < Time.now
+      yield t
+      t += 1.month
+    end
   end
 end
 
